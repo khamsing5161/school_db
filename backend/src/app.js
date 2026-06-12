b1b0cf438fa664aed1785022b24913f91c24a99e
khamsing5161 <thasing436@gmail.com>
2026-06-12 14:26:52 +0700

    me

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,16 @@
-const express  = require('express');
-const cors     = require('cors');
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://singkham.netlify.app/"
+    "https://singkham.netlify.app"
   ]
 }));
 
-app.use(express.json()); // รับ JSON จาก request body
+app.use(express.json());
 
 // Routes
 app.use('/students',  require('./routes/students'));
